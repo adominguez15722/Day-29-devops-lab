d992b34bde5b40ee99ee73ccc3f073ccd7174de6
Anthony <adominguez15722@gmail.com>
2022-02-10 16:12:48 -0600

    rollbar required

--- public/index.js
+++ public/index.js
@@ -1,3 +1,6 @@
+const Rollbar = require("rollbar")
+
 let btn = document.querySelector('button')
 btn.addEventListener('click', () => {
+    Rollbar.log('this is an error')
     console.log('should not have pressed that')})
