c3b6cd3ee9896dce211719b19cedbc83deeac705
Anthony <adominguez15722@gmail.com>
2022-02-10 14:36:00 -0600

    heroku ready

--- server/server.js
+++ server/server.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.html'))
+})
+
+
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`server running on ${port}`))
